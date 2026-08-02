6654dbb5d55a1dd639032f10d8db67aefcf0a25d
Jnanesh095 <janumeesala555@gmail.com>
2026-08-02 14:31:09 +0530

    Remove Memories section and cleanup gallery code

--- script.js
+++ script.js
@@ -120,47 +120,8 @@ function createHeart(){
 
 }
 
-// -----------------------------
-// Image Click Zoom
-// -----------------------------
-document.querySelectorAll(".images img").forEach(img=>{
 
-    img.addEventListener("click",()=>{
 
-        const overlay=document.createElement("div");
-
-        overlay.style.position="fixed";
-        overlay.style.top=0;
-        overlay.style.left=0;
-        overlay.style.width="100%";
-        overlay.style.height="100%";
-        overlay.style.background="rgba(0,0,0,.9)";
-        overlay.style.display="flex";
-        overlay.style.justifyContent="center";
-        overlay.style.alignItems="center";
-        overlay.style.cursor="pointer";
-        overlay.style.zIndex="10000";
-
-        const image=document.createElement("img");
-
-        image.src=img.src;
-        image.style.maxWidth="90%";
-        image.style.maxHeight="90%";
-        image.style.borderRadius="20px";
-
-        overlay.appendChild(image);
-
-        document.body.appendChild(overlay);
-
-        overlay.onclick=()=>{
-
-            overlay.remove();
-
-        };
-
-    });
-
-});
 
 // -----------------------------
 // Mouse Glow
